75812be73a7fb270ab462cb9076544256e435c00
ModestCloud <zhilong.chen@oracle.com>
2014-10-14 16:22:04 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7e07f0f6a85f953d224188128feddd56a71cfc853f7cdb9ae80bdeed2de1084f74cfcff02e47190b3f7e43d7379116a846fc2860d3e57df928d8d61f5b80904e'
