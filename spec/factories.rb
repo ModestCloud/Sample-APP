8eafd80e88239356944e2f0ded228261d3948ed7
ModestCloud <zhilong.chen@oracle.com>
2014-10-20 13:49:03 -0700

    Finish sign in

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
 	factory :user do
-		name     "Michael Hartl"
-		email    "michael@example.com"
-		password "foobar"
-		password_confirmation "foobar"
+		name     "test"
+		email    "test@test.com"
+		password "woshitest"
+		password_confirmation "woshitest"
 	end
 end
